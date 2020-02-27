6341da725b1a040cafff23afd6f91b89502d123e
trelL <aegis.trellaren@gmail.com>
2020-02-26 22:02:38 -0600

    -removed InputPage class from main.dart -removed _InputPageState from main.dart +added InputPage class to input_page.dart +added _InputPageState to input_page.dart +added some theme data to main.dart -hid the debug flag.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,32 +7,13 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF090C22),
+        accentColor: Color(0xFF1C2033),
+        scaffoldBackgroundColor: Color(0xFF07091B),
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
